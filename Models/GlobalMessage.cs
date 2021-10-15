a7e534df41feb756f27f6d93660c21463ea9eb5d
Nathan Mac <nathan.mac@rumbleentertainment.com>
2021-10-14 17:23:24 -0700

    globalmessage model, need to check message parameters

--- Models/GlobalMessage.cs
+++ Models/GlobalMessage.cs
@@ -7,12 +7,31 @@ using Rumble.Platform.Common.Web;
 
 namespace Rumble.Platform.MailboxService.Models
 {
-    public class GlobalMessage
+    public class GlobalMessage : Message
     {
+        internal const string DB_KEY_ELIGIBLE_FOR_NEW_ACCOUNTS = "elgb";
+        internal const string DB_KEY_ATTACHMENTS = "attchmnts";
+
+        public const string FRIENDLY_KEY_ELIGIBLE_FOR_NEW_ACCOUNTS = "eligibleForNewAccounts";
+        public const string FRIENDLY_KEY_ATTACHMENTS = "attachments";
+        
+        [BsonElement(DB_KEY_ELIGIBLE_FOR_NEW_ACCOUNTS)]
+        [JsonProperty(PropertyName = FRIENDLY_KEY_ELIGIBLE_FOR_NEW_ACCOUNTS)]
+        public bool EligibleForNewAccounts { get; private set; }
+        
+        [BsonElement(DB_KEY_ATTACHMENTS)]
+        [JsonProperty(PropertyName = FRIENDLY_KEY_ATTACHMENTS)]
+        public List<Attachment> Attachments { get; private set; }
+
+        public GlobalMessage(bool eligibleForNewAccounts, List<Attachment> attachments)
+        {
+            EligibleForNewAccounts = eligibleForNewAccounts;
+            Attachments = attachments;
+        }
         
     }
 }
 
 // GlobalMessage : Message
 // - EligibleForNewAccounts (bool)
-// - Attachment
+// - Attachment - reasonable to think this should be a collection of attachments instead?
