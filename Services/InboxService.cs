0878832837e71ec73ffd58b3c791c85da6742ee9
Nathan Mac <nathan.mac@rumbleentertainment.com>
2021-10-15 12:24:24 -0700

    inbox service wip

--- Services/InboxService.cs
+++ Services/InboxService.cs
@@ -1,11 +1,31 @@
-using Rumble.Platform.Common.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Timers;
+using MongoDB.Driver;
 using Rumble.Platform.MailboxService.Models;
+using Rumble.Platform.Common.Utilities;
+using Rumble.Platform.Common.Web;
+using Rumble.Platform.CSharp.Common.Interop;
+using Rumble.Platform.MailboxService.Models;
+using Timer = System.Timers.Timer;
 
 namespace Rumble.Platform.MailboxService.Services
 {
     public class InboxService : PlatformMongoService<Inbox>
     {
-        public InboxService() : base(collection: "inboxes") {  }
+        private Timer _inboxTimer;
+
+        public InboxService() : base(collection: "inboxes")
+        {
+            _inboxTimer = new Timer(interval: int.Parse(RumbleEnvironment.Variable("INBOX_CHECK_FREQUENCY_SECONDS") ?? "10"))
+            {
+                AutoReset = true
+            };
+            _inboxTimer.Elapsed += CheckExpiredInbox;
+            _inboxTimer.Start();
+        }
     }
 }
 
