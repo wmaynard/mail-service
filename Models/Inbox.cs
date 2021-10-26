cffe7cc3d446e5998bd56d9768ee184b1062cd30
Nathan Mac <nathan.mac@rumbleentertainment.com>
2021-10-25 17:33:47 -0700

    inbox model change to include timestamp

--- Models/Inbox.cs
+++ Models/Inbox.cs
@@ -9,9 +9,11 @@ namespace Rumble.Platform.MailboxService.Models
     {
         internal const string DB_KEY_ACCOUNT_ID = "aid";
         internal const string DB_KEY_MESSAGES = "msgs";
+        internal const string DB_KEY_TIMESTAMP = "tmestmp";
 
         public const string FRIENDLY_KEY_ACCOUNT_ID = "accountId";
         public const string FRIENDLY_KEY_MESSAGES = "messages";
+        public const string FRIENDLY_KEY_TIMESTAMP = "timestamp";
         
         [BsonElement(DB_KEY_ACCOUNT_ID)]
         [JsonProperty(PropertyName = FRIENDLY_KEY_ACCOUNT_ID)]
@@ -20,11 +22,16 @@ namespace Rumble.Platform.MailboxService.Models
         [BsonElement(DB_KEY_MESSAGES)]
         [JsonProperty(PropertyName = FRIENDLY_KEY_MESSAGES)]
         public List<Message> Messages { get; private set; }
+        
+        [BsonElement(DB_KEY_TIMESTAMP)]
+        [JsonProperty(PropertyName = FRIENDLY_KEY_TIMESTAMP)]
+        public long? Timestamp { get; private set; }
 
-        public Inbox(string aid, List<Message> messages)
+        public Inbox(string aid, List<Message> messages, long? timestamp = null)
         {
             AccountId = aid;
             Messages = messages;
+            Timestamp = timestamp;
         }
 
         public void UpdateMessages(List<Message> messages)
